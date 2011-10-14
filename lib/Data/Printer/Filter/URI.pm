39a49b86443ac3a8f8aacd68a5398cb1873ae731
Stanislaw Pusep <stanislav.poussep@buscape-inc.com>
2011-10-14 18:08:06 -0300

    INIT

---
+++ lib/Data/Printer/Filter/URI.pm
@@ -0,0 +1,112 @@
+package Data::Printer::Filter::URI;
+# ABSTRACT: pretty-printing URI objects
+
+use strict;
+use utf8;
+use warnings 'all';
+
+use Data::Printer::Filter;
+use Term::ANSIColor;
+
+# VERSION
+
+=head1 SYNOPSIS
+
+In your program:
+
+    use Data::Printer filters => {
+        -external => [ 'URI' ],
+    };
+
+or, in your C<.dataprinter> file:
+
+    {
+        filters => {
+            -external => [ 'URI' ],
+        },
+    };
+
+You can also setup color and display details:
+
+    use Data::Printer {
+        filters => {
+            -external   => [ 'URI' ],
+        }, color => {
+            uri         => 'bright_yellow',
+        },
+    };
+
+=head1 DESCRIPTION
+
+This is a filter plugin for L<Data::Printer>.
+It filters through several L<URI> manipulation classes and displays the L<URI> as a string.
+
+=head2 Parsed Protocols
+
+=for :list
+* data
+* file
+* ftp
+* gopher
+* http
+* https
+* ldap
+* ldapi
+* ldaps
+* mailto
+* mms
+* news
+* nntp
+* pop
+* rlogin
+* rsync
+* rtsp
+* rtspu
+* sftp
+* sip
+* sips
+* snews
+* ssh
+* telnet
+* tn3270
+* urn
+
+=cut
+
+our @schemes = qw(
+    URL
+    data
+    file
+    ftp
+    gopher
+    http
+    https
+    ldap
+    ldapi
+    ldaps
+    mailto
+    mms
+    news
+    nntp
+    pop
+    rlogin
+    rsync
+    rtsp
+    rtspu
+    sftp
+    sip
+    sips
+    snews
+    ssh
+    telnet
+    tn3270
+    urn
+);
+
+filter "URI::$_" => sub {
+    my ($obj, $p) = @_;
+    my $color = $p->{color}{uri};
+    return colored('"' . $obj->as_string . '"', $color // 'bright_yellow');
+} for @schemes;
+
+1;
